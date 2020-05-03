901817de12989a782a719f7e6267bad4e3155878
yukineko <hideki030403@gmail.com>
2020-05-03 12:54:54 +0900

    update anti-ss.js

--- src/anti-ss.js
+++ src/anti-ss.js
@@ -4,21 +4,23 @@ fetch('https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js').then(r
 })
 
 function main() {
-    for(var i = 0; hws.length > i; i++) {
-        var hw = $(hws[i])
-        if(findNotYet(hw)) {
-            hw.children('a')[0].click()
-            setTimeout(function() {
-                $('.bmpui-image')[0].click()
-                $('video').on('ended', function() {main()})
-            }, 10000)
-            break
+    setTimeout(function() {
+        for(var i = 0; hws.length > i; i++) {
+            var hw = $(hws[i])
+            if(findNotYet(hw)) {
+                hw.children('a')[0].click()
+                setTimeout(function() {
+                    $('.bmpui-image')[0].click()
+                    $('video').on('ended', function() {main()})
+                }, 2500)
+                break
+            }
+    
+            if(hws.length === i) {
+                alert('完了')
+            }
         }
-
-        if(hws.length === i) {
-            alert('完了')
-        }
-    }
+    }, 2500)
 }
 
 function findNotYet(hw) {
